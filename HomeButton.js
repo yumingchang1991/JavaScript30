07d82e6e88c88652e6211ec0494b41dd250e78fa
ChillDiver <yumingchang1991@gmail.com>
2022-05-03 16:18:10 +0800

    complete homeButton to be placed on top without changing viewport height of application

--- HomeButton.js
+++ HomeButton.js
@@ -1,14 +1,39 @@
 function addHomeButton() {
   const Body = document.querySelector('body')
-  const HomeButton = returnHomeButtonElement()
-  Body.insertBefore(HomeButton , Body.firstChild)
+  const ButtonContainer = returnContainer()
+  Body.insertBefore(ButtonContainer, Body.firstChild)
+}
+
+function returnContainer() {
+  const Container = document.createElement('div')
+  Container.style.position = 'fixed'
+  Container.style.height = '25vh'
+  Container.style.textAlign = 'center'
+  Container.className = 'home-button-container'
+  Container.appendChild(returnHomeButtonElement())
+  return Container
 }
 
 function returnHomeButtonElement() {
   const homeButton = document.createElement('button')
-  homeButton.style.background = 'red'
-  homeButton.textContent = 'Go Back'
+  homeButton.style.width = '100vw'
+  homeButton.style.background = '#e6e6e6'
+  homeButton.style.paddingInline = '1rem'
+  homeButton.style.paddingBlock = '0.7rem'
+  homeButton.style.border = 'none'
+  homeButton.appendChild(returnLinkToHome())
   return homeButton
+}
+
+function returnLinkToHome() {
+  const linkToHome = document.createElement('a')
+  linkToHome.text = '<< Go Back'
+  linkToHome.href = '../index.html'
+  linkToHome.style.textDecoration = 'none'
+  linkToHome.style.fontFamily = 'Calibri'
+  linkToHome.style.fontWeight = 'bold'
+  linkToHome.style.fontSize = '3rem'
+  return linkToHome
 }
 
 addHomeButton()
