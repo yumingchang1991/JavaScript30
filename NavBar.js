fabdf09ff597fb8d539ee7d31d68d52ebe0eb95e
ChillDiver <yumingchang1991@gmail.com>
2022-05-04 21:28:56 +0800

    change file name to NavBar for better understanding

---
+++ NavBar.js
@@ -0,0 +1,38 @@
+function addHomeButton() {
+  const Body = document.querySelector('body')
+  Body.insertBefore(returnNavBar(), Body.firstChild)
+}
+
+function returnNavBar() {
+  const NavBar = document.createElement('nav')
+  // position
+  NavBar.style.position = 'fixed'
+  // box size
+  NavBar.style.width = '100vw'
+  NavBar.style.margin = 0
+  // background
+  NavBar.style.background = '#e6e6e6'
+  NavBar.style.opacity = '0.7'
+  NavBar.appendChild(returnLinkToHome())
+
+  return NavBar
+}
+
+function returnLinkToHome() {
+  const linkToHome = document.createElement('a')
+  linkToHome.text = '<< Go Back'
+  linkToHome.href = '../index.html'
+  linkToHome.style.display = 'inline-block'
+  linkToHome.style.width = '100%'
+  linkToHome.style.color = 'black'
+  
+  linkToHome.style.textDecoration = 'none'
+  linkToHome.style.textAlign = 'center'
+  linkToHome.style.fontFamily = 'Calibri'
+  linkToHome.style.fontWeight = 'bold'
+  linkToHome.style.fontSize = '4em'
+  
+  return linkToHome
+}
+
+addHomeButton()
