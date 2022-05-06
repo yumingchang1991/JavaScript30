5a775640e69be825140e6b197c19691553f41dde
ChillDiver <yumingchang1991@gmail.com>
2022-05-06 17:20:14 +0800

    separate style from .js to .css and make it responsive

--- NavBar.js
+++ NavBar.js
@@ -1,3 +1,15 @@
+addHomeButton()
+addNavBarCSS()
+
+function addNavBarCSS() {
+  const Head = document.getElementsByTagName('HEAD')[0]
+  const link = document.createElement('link')
+  link.href = '../NavBar.css'
+  link.type = 'text/css'
+  link.rel = 'stylesheet'
+  Head.appendChild(link)
+}
+
 function addHomeButton() {
   const Body = document.querySelector('body')
   Body.insertBefore(returnNavBar(), Body.firstChild)
@@ -5,36 +17,17 @@ function addHomeButton() {
 
 function returnNavBar() {
   const NavBar = document.createElement('nav')
-  // position
-  NavBar.style.position = 'fixed'
-  NavBar.style.top = 0
-  NavBar.style.left = 0
-  // box size
-  NavBar.style.width = '100vw'
-  NavBar.style.margin = 0
-  // background
-  NavBar.style.background = '#e6e6e6'
-  NavBar.style.opacity = '0.7'
+  NavBar.className = 'nav-bar'
   NavBar.appendChild(returnLinkToHome())
-
   return NavBar
 }
 
 function returnLinkToHome() {
   const linkToHome = document.createElement('a')
-  linkToHome.text = '<< Go Back'
+  linkToHome.className = 'nav-bar-link'
+  linkToHome.text = 'Go Back'
   linkToHome.href = '../index.html'
-  linkToHome.style.display = 'inline-block'
-  linkToHome.style.width = '100%'
-  linkToHome.style.color = 'black'
-  
-  linkToHome.style.textDecoration = 'none'
-  linkToHome.style.textAlign = 'center'
-  linkToHome.style.fontFamily = 'Calibri'
-  linkToHome.style.fontWeight = 'bold'
-  linkToHome.style.fontSize = '4em'
   
   return linkToHome
 }
 
-addHomeButton()
